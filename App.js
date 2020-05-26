9cb098a83568ab15ae8eba71d1b3bfcdfcc8d25c
Margot <margaretsobota@gmail.com>
2020-05-26 11:53:54 -0700

    camera permissions works

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component }from 'react';
+import ReactDOM from "react-dom";
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+
+
+class CameraApp extends Component {
+  state = {
+    hasPermission: null,
+    type: Camera.Constants.Type.back,
+  }
+  
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasPermission: status === 'granted' });
+  }
+
+  render(){
+    const { hasPermission } = this.state;
+
+    if (hasPermission === null) {
+      return <View />;
+    } else if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.cameraType}>
+            
+          </Camera>
+        </View>
+      );
+    }
+  }
+
+}
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <CameraApp style={styles.container}> </CameraApp>
   );
 }
 
